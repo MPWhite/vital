98969e8d2e83c299c50efedf4a5b6546b88ad381
Matt <mattp.white95@gmail.com>
2022-11-30 01:11:39 -0500

    Dark mode boulder map

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import React from "react";
-import BoulderMap from "./layouts/BoulderMap";
 import BoulderUpload from "./layouts/BoulderUpload/BoulderUpload";
 import { BoulderPage } from "./layouts/BoulderPage/BoulderPage";
 import { UserPage } from "./layouts/UserPage/UserPage";
 import styled from "styled-components";
+import BoulderMap from "./layouts/BoulderMap/BoulderMap";
 
 function App() {
   return (
     <div>
       {/*<BoulderPage />*/}
-      <UserPage />
+      {/*<UserPage />*/}
+      <BoulderMap />
     </div>
   );
 }
--- src/layouts/BoulderMap/BoulderMap.js
+++ src/layouts/BoulderMap/BoulderMap.js
@@ -1,16 +1,20 @@
 import React, { useState } from "react";
-import "./BoulderMap.scss";
 import CardCarousel from "../../components/CardCarousel";
 import GymMap from "../../components/GymMap";
+import styled from "styled-components";
 
-const BoulderMap = () => {
+const BoulderMapWrapper = styled.div`
+  padding: 0;
+  background-color: black;
+`;
+
+export const BoulderMap = () => {
   const [activePinId, setActivePinId] = useState(1);
   return (
-    <div className="BoulderMap">
-      <div className="BoulderFilter">TODO - add filter block</div>
+    <BoulderMapWrapper>
       <GymMap activePinId={activePinId} />
       <CardCarousel activePinId={activePinId} setActivePinId={setActivePinId} />
-    </div>
+    </BoulderMapWrapper>
   );
 };
 
